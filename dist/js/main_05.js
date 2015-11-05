d36e3d604b9a5775dd27d174f3c9042d55f24781
Christoph Forster <christoph.forster@gmail.com>
2015-11-05 04:56:35 -0800

    bsp_05 - shadow effect is now configurable or default can be used

---
+++ dist/js/main_05.js
@@ -0,0 +1,10 @@
+jQuery(function ($) {
+    "use strict";
+
+    $("h2").shadow({
+        copies: 5,
+        opacity: 0.1,
+        offset: 0.3
+    }).css("color", "orange");
+
+});
---
+++ dist/js/plugins_05.js
@@ -0,0 +1,55 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+(function ($) {
+    "use strict";
+
+
+    $.fn.shadow = function (options) { // .fn erzeugt Prototype, welcher für alle elemente nutzbar ist
+        var defaults = {
+            copies: 5,
+            opacity: 0.1,
+            offset: 1
+        }
+        options = $.extend(defaults, options);
+        return this.each(function () {
+            var i,
+                $original = $(this);
+
+            for (i = 1; i <=options.copies; i+=options.offset) {
+                $original.clone()
+                    .css({
+                        position: 'absolute',
+                        top: $original.offset().top + i,
+                        left: $original.offset().left + i,
+                        margin: 0,
+                        zIndex : -1,
+                        opacity: options.opacity
+                    })
+            .appendTo("body");
+            }
+        });
+    };
+}(jQuery));
